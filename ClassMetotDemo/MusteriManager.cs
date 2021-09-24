c2fca484d15a28779a5d5a6266d82a6ef6f4a4a3
celikarda812 <celikarda812@gmail.com>
2021-09-24 08:01:48 -0500

    ödev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Musteriekle(Musteri musteri1)
+        {
+            Console.WriteLine("musteri " + musteri1.Musteri_adi + " eklendi");
+
+
+        }
+
+        public void Musterisil(Musteri musteri1)
+        {
+
+
+            Console.WriteLine("musteri" + musteri1.Musteri_adi + "silindi");
+
+
+        }
+
+        public void MusteriListele( Musteri[] musteriler)
+        {
+
+
+            foreach (Musteri item in musteriler)
+            {
+                Console.WriteLine("musteri adi :" + item.Musteri_adi);
+                Console.WriteLine(" musteri yas :" + item.yas);
+                Console.WriteLine("********************************************");
+
+
+            }
+
+        }
+
+        internal void MusteriListele()
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,18 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            musteri1.Musteri_adi = "arda celik";
+            musteri1.yas = 24;
+            musteri2.Musteri_adi = " samet eralp";
+            musteri2.yas = 34;
+            MusteriManager methods = new MusteriManager();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2};
+
+            methods.MusteriListele(musteriler);   
+            
+
         }
     }
 }
